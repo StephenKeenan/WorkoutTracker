763e6c813ac061219b4dd6214a4237d5a5aa3450
Stephen Keenan <stephenkeenan02@gmail.com>
2021-01-23 22:39:07 -0500

    server.js

--- seeders/server.js
+++ seeders/server.js
@@ -10,14 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`Running: http://localhost: ${PORT}!`);
 });
